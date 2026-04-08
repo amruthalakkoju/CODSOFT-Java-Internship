76691a7348cc6eec6457ab9437de5132b30a0c42
amruthalakkoju <amruthahasini1257@gmail.com>
2026-04-08 19:16:58 +0530

    Add files via upload

---
+++ CurrencyConverter.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+public class CurrencyConverter{
+public static void main(String[] args) {
+Scanner sc=new Scanner(System.in);
+double amount,convertedAmount;
+int choice;
+System.out.println("---Currency Converter---");
+System.out.println("1. INR to USD");
+System.out.println("2. USD to INR");
+System.out.println("3. INR to EUR");
+System.out.println("4. EUR to INR");
+System.out.print("Choose conversion: ");
+choice=sc.nextInt();
+System.out.print("Enter amount: ");
+amount=sc.nextDouble();
+switch(choice){
+case 1:
+convertedAmount=amount*0.012;
+System.out.println("Converted Amount: "+convertedAmount);
+break;
+case 2:
+convertedAmount=amount*83.0;
+System.out.println("Converted Amount: "+convertedAmount);
+break;
+case 3:
+convertedAmount=amount*0.011;
+System.out.println("Converted Amount: "+convertedAmount);
+break;
+case 4:
+convertedAmount=amount*90.0;
+System.out.println("Converted Amount: "+convertedAmount);
+break;
+default:
+System.out.println("Invalid choice!");
+}
+sc.close();
+}
+}
